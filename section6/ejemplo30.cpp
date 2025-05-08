312f8242f1066ba871a1dfd8235a9a809f533d50
jonistyle2023 <jonathanpan22@gmail.com>
2025-05-08 16:42:55 -0500

    upload exercite 30

---
+++ section6/ejemplo30.cpp
@@ -0,0 +1,35 @@
+//
+// Created by jonathan panchana on 8/5/2025.
+//
+
+/* Procedimiento Gotoxy
+ * Curso: Software 2/2
+ */
+
+// Bloque de declaraciones
+// Definición de las librerias
+#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+// Espacio de Nombres
+using namespace std;
+// Definición de variables globales
+int i, j, k;
+// Prototipo del procedimiento
+void gotoxy(int x, int y); // X=columna, Y=fila
+
+// Función principal
+int main() {
+    gotoxy(10,8);cout << "Hello World!";
+}
+
+// Declaración de los procedimientos
+void gotoxy(int x,int y){
+    HANDLE hcon;
+    hcon = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD dwPos;
+    dwPos.X = x;  // Columna
+    dwPos.Y= y;  // Fila
+    SetConsoleCursorPosition(hcon,dwPos);
+}
+
