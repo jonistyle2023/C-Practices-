4fe6b32f9e5d485e70d59518aa298100eae7acc5
jonistyle2023 <jonathanpan22@gmail.com>
2025-06-05 15:45:46 -0500

    add archives exercites

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/jonat/CLionProjects/fundamentosProgramacion
+# Install script for directory: C:/Users/jonat.COSMOS-PC/CLionProjects/C-Practices-
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,13 +34,13 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Users/jonat/AppData/Local/Programs/CLion/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Users/jonat.COSMOS-PC/AppData/Local/Programs/CLion/bin/mingw/bin/objdump.exe")
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/jonat/CLionProjects/fundamentosProgramacion/cmake-build-debug/install_local_manifest.txt"
+  file(WRITE "C:/Users/jonat.COSMOS-PC/CLionProjects/C-Practices-/cmake-build-debug/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -56,6 +56,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/jonat/CLionProjects/fundamentosProgramacion/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/jonat.COSMOS-PC/CLionProjects/C-Practices-/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
---
+++ section8/A_EJ01.cpp
@@ -0,0 +1,79 @@
+//
+// Created by Jonathan Panchana Rodríguez on 5/6/2025.
+//
+
+/* Aplicar el concepto de diccionario*
+ * Curso: Software 2/2.
+ */
+
+// Bloque de declaraciones
+// Definir librerias
+#include <iostream>
+#include <map>
+// Espacio de nombres
+using namespace std;
+// Variables y constantes globales
+char Nom[30], Nb[30];
+int Edad, Lim, i; // Contador para que vayan identificando los elementos
+// Declaración del diccionario
+map<string, int> Sistema;
+
+// Definir un variable tipo iterador
+map<string, int>::iterator it;
+
+// Bloque de instrucciones
+// Función principal
+int main() {
+    //Ingreso de cantidad de elementos al diccionario
+
+    cout << "Ingrese el número de elementos: ";
+    cin >> Lim;
+    getchar();
+    //Ingresamos elementos al diccionario
+    for (i = 1; i <= Lim; i++) {
+        cout << "Ingrese Nombre[" << i << "]: ";
+        gets(Nom);
+        cout << "Ingrese el edad[" << i << "]: ";
+        cin >> Edad;
+        getchar();
+        // Agregar elementos al diccionario
+        Sistema.insert(make_pair(Nom, Edad));
+    }
+    //Datos de Salida
+    cout << "*********************************\n";
+    cout << "***** MOSTRAR DICCIONARIO *******\n";
+    cout << "*********************************\n";
+    // Recorrer todos los elementos del diccionario
+    for (it = Sistema.begin(); it != Sistema.end(); ++it) {
+        cout << it->first << ":" << it->second << "\n";
+    }
+
+    //Datos de Salida
+    cout << "*********************************\n";
+    cout << "*** BUSCAR ELEMENTO POR NOMBRE **\n";
+    cout << "*********************************\n";
+    cout << "\nIngrese el nombre a buscar: ";
+    gets(Nb);
+    // Buscar un elemento en el diccionario
+    it = Sistema.find(Nb);
+    if (it != Sistema.end()) {
+        cout << "\nLa edad de " << Nb << " es: " << Sistema[Nb];
+    }
+
+    // Eliminar un elemento del diccionario
+    cout << "\n\nIngrese el nombre a eliminar: ";
+    gets(Nb);
+    Sistema.erase(Nb);
+    cout << "\nEliminado elemento....\n";
+    //Datos de Salida
+    cout << "*********************************\n";
+    cout << "*MOSTRAR DICCIONARIO ACTUALIZADO*\n";
+    cout << "*********************************\n";
+    // Recorrer todos los elementos del diccionario
+    for (it = Sistema.begin(); it != Sistema.end(); ++it) {
+        cout << it->first << ":" << it->second << "\n";
+    }
+    //pausa
+    cout << "\n\n";
+    system("pause");
+}
